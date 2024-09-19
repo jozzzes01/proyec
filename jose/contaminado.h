b4e218565995976b3a14c05549d16aa35e38b561
jozzzes01 <josechavezr0109@gmail.com>
2024-09-19 15:46:01 -0500

    Create contaminado.h

---
+++ jose/contaminado.h
@@ -0,0 +1,26 @@
+#include "posicion.h"
+
+
+
+class bas :public posicion {
+
+protected:
+
+	bool solido = false;
+	char basura_;
+
+
+public:
+
+	//ubicacion aleatoria de la basura
+	bas():posicion(){
+		basura_ = 'a';
+	}
+
+	void mostrarbasura() {
+		Console::SetCursorPosition(x, y);
+		cout << basura_;
+	}
+
+};
+
